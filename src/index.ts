379593c0619a958bb68113b5bebda4b2d53e6c87
Davorin Šego <davorin.sego@gmail.com>
2025-11-14 12:24:03 +0100

    Include better auth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
 import { Elysia } from "elysia";
+import { openapi } from '@elysiajs/openapi'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { auth } from './lib/auth'
+
+
+const authMiddleware = new Elysia({ name: 'better-auth' })
+  .mount(auth.handler)
+  .macro({
+    auth: {
+      async resolve({ status, request: { headers } }) {
+        console.log(headers)
+        const session = await auth.api.getSession({
+          headers
+        })
+        if (!session) return status(401)
+
+        return {
+          user: session.user,
+          session: session.session
+        }
+      }
+    }
+  })
+
+const app = new Elysia()
+  .use(openapi())
+  .use(authMiddleware)
+  .get("/", () => "Hello Elysia")
+  .get('/user', ({ user }) => user, {
+    auth: true
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
